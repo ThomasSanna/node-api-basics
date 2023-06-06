48ccd8849fc5eace13624ad544c03faea943c5d2
WadeeKT <thomassanna1e@gmail.com>
2023-06-06 14:21:47 +0200

    Adding Express and Nodemon

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get("/", (req, res) => res.send('Hello Express 2 !'))
+
+
+
+app.listen(port, () => console.log(`Notre application Node est démarée sur : http://localhost:${port}`))
+
