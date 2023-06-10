1820996d6e7706f6e8c2206d9a9a9fa3255652ea
WadeeKT <thomassanna1e@gmail.com>
2023-06-10 22:41:44 +0200

    Advanced search with sequelize

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,21 +1,32 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
+
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    if(req.query.name) { // localhost:5000/api/pokemons?name=.../ Advanced search
+    if(req.query.name) { // localhost:3000/api/pokemons?name=.../ Advanced search
       const name = req.query.name
-      return Pokemon.findAll({ where: { name: name }})
-        .then(pokemons => {
-          const message = `There are ${pokemons.length} pokemons with the name ${name}`
-          res.json({ message, data: pokemons })
+
+      if(name.length < 2) { // if the name is less than 2 characters, return an error (to avoid too many results)
+        return res.status(400).json({ message: 'The name must contain at least 2 characters.' })
+      } else {
+        return Pokemon.findAndCountAll({
+          where: { 
+            name: { // 'name' is the column name in the table
+              [Op.like]: `%${name}%` // 'Op.like' is the operator for 'LIKE' in SQL
+            } 
+          },
+          order: ['name'],
+          limit: parseInt(req.query.limit) || 5 // limit the number of results (localhost:3000/api/pokemons?name=...&limit=10)
         })
-        .catch(error => {
-          const message = `There is no pokemon with the name ${name}`
-          res.status(404).json({ message, data: error })
-        })
+          .then(({count, rows}) => {
+            const message = `There are ${count} pokemons with the name ${name}.`
+            res.json({ message, data: rows })
+          })
+      }
     } 
     else{
-      Pokemon.findAll()
+      Pokemon.findAll({ order: ['name'] })
         .then(pokemons => {
           const message = 'List of Pokemons has been found.'
           res.json({ message, data: pokemons })
